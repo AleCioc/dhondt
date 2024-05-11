8ccec0bb1dbd2515bb33dd54844befdc25aaf51c
alecioc <alessandro@ciociola.me>
2024-05-11 12:02:52 +0200

    fix dhondt calculations

--- Home.py
+++ Home.py
@@ -3,6 +3,7 @@ import streamlit as st
 
 st.title("Calcolo seggi")
 
+
 def dhont(nSeats, votes, verbose=False):
     """
     nSeats is the number of seats
@@ -45,17 +46,21 @@ votes_dict = {
 }
 
 
-votes_dict = pd.Series(votes_dict)
+votes_dict = pd.Series(votes_dict, name="n_voti")
 
-cols = st.columns((1, 1))
-cols[0].subheader("Imposta numero voti")
-cols[0].experimental_data_editor(votes_dict)
+st.header("Liste")
+
+cols = st.columns((1, 1, 1))
+
+cols[0].subheader("Imposta voti")
+votes_dict = cols[0].experimental_data_editor(votes_dict)
+cols[0].write(f"Tot voti: {votes_dict.sum()}")
+
 
 coalitions_dict = {
     "CENTRODESTRA": ["FDI", "XBENE", "FORZAITALIA", "MASTRANGELO"],
     "CENTROSINISTRA": ["PD", "BOTTEGA", "PROCINO"],
     "M5S": ["PRODIGIO", "M5S"],
-
 }
 
 coalitions_votes = {}
@@ -64,21 +69,50 @@ for c in coalitions_dict:
     for l in coalitions_dict[c]:
         coalitions_votes[c] += votes_dict[l]
 
-dhont_results = dhont(nSeats=16, votes=votes_dict.to_dict(), verbose=False)
-cols[1].subheader("Seggi assegnati")
-cols[1].write(dhont_results)
+dhont_results_coalizioni = dhont(nSeats=16, votes=coalitions_votes, verbose=False)
+
+dhont_results_by_list = dict()
+for coalizione in coalitions_dict:
+    votes_coalizione_by_list = {k: votes_dict[k] for k in votes_dict.index if k in coalitions_dict[coalizione]}
+    _dhont_results_by_list = dhont(
+        nSeats=dhont_results_coalizioni[coalizione],
+        votes=votes_coalizione_by_list,
+        verbose=False
+    )
+    dhont_results_by_list = dhont_results_by_list | _dhont_results_by_list
+
+
+cols[1].subheader("Percentuali")
+cols[1].dataframe(
+    (((pd.Series(votes_dict) / pd.Series(votes_dict).sum()) * 100).round(2).astype(str) + " %").rename(
+        "%_voti"
+    )
+)
+
+cols[2].subheader("Seggi")
+cols[2].write(pd.Series(dhont_results_by_list))
+sum_seats = pd.Series(dhont_results_by_list).sum()
+cols[2].write(f"Tot seggi: {sum_seats}")
+
+st.header("Coalizioni")
 
 cols = st.columns((1, 1, 1))
 
-cols[0].subheader("Percentuali liste")
-cols[0].dataframe(
-    ((pd.Series(votes_dict) / pd.Series(votes_dict).sum()) * 100).round(2).astype(str) + " %"
+cols[0].subheader("Voti")
+cols[0].dataframe(pd.Series(coalitions_votes))
+tot_votes = pd.Series(coalitions_votes).sum()
+cols[0].write(f"Tot seggi: {tot_votes}")
+
+
+cols[1].subheader("Percentuali")
+cols[1].dataframe(
+    (((pd.Series(coalitions_votes) / pd.Series(coalitions_votes).sum()) * 100).round(2).astype(str) + " %").rename(
+        "%_voti"
+    )
 )
 
-cols[1].subheader("Voti coalizione")
-cols[1].dataframe(pd.Series(coalitions_votes))
+cols[2].subheader("Seggi")
+cols[2].dataframe(pd.Series(dhont_results_coalizioni))
+tot_seats = pd.Series(dhont_results_coalizioni).sum()
+cols[2].write(f"Tot seggi: {tot_seats}")
 
-cols[2].subheader("Percentuali")
-cols[2].dataframe(
-    ((pd.Series(coalitions_votes) / pd.Series(coalitions_votes).sum()) * 100).round(2).astype(str) + " %"
-)
