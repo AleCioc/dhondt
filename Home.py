32eb2283c9d1116e918cf5caf846fa1cae7a15f7
alecioc <alessandro@ciociola.me>
2024-05-15 15:53:05 +0200

    update

--- Home.py
+++ Home.py
@@ -4,44 +4,53 @@ import streamlit as st
 st.title("Calcolo seggi")
 
 
-def dhont(nSeats, votes, verbose=False):
+def dhondt(nSeats, votes, verbose=False):
     """
     nSeats is the number of seats
     votes is a dictionary with the key:value {'party':votes}
     verbose is an option to print designation info
     """
-    t_votes=votes.copy()
-    seats={}
-    for key in votes: seats[key]=0
+
+    t_votes = votes.copy()
+    seats = {}
+
+    for key in votes:
+        seats[key] = 0
+
+    # with st.expander("Dhondt procedimento completo"):
+
     while sum(seats.values()) < nSeats:
-        max_v= max(t_votes.values())
-        next_seat=list(t_votes.keys())[list(t_votes.values()).index(max_v)]
+        max_v = max(t_votes.values())
+        next_seat = list(t_votes.keys())[list(t_votes.values()).index(max_v)]
         if next_seat in seats:
-            seats[next_seat]+=1
+            seats[next_seat] += 1
         else:
-            seats[next_seat]=1
+            seats[next_seat] = 1
 
         if verbose:
-            print("{} Escaño: {}".format(sum(seats.values()),next_seat))
+            print("Seggio {}: {}".format(sum(seats.values()), next_seat))
             for key in t_votes:
-                print("\t{} [{}]: {:.1f}".format(key,seats[key],t_votes[key]))
+                print("\t{} [{}]: {:.1f}".format(key,seats[key], t_votes[key]))
             print("\b")
-        t_votes[next_seat]=votes[next_seat]/(seats[next_seat]+1)
+
+        t_votes[next_seat] = votes[next_seat]/(seats[next_seat]+1)
+
     return seats
+
 
 votes_dict = {
 
-    "FDI": 2500,
+    "FDI": 2600,
     "XBENE": 2900,
     "FORZAITALIA": 2600,
-    "MASTRANGELO": 1545,
+    "MASTRANGELO": 1500,
 
-    "PD": 1400,
-    "BOTTEGA": 1200,
-    "PROCINO": 400,
+    "PD": 1300,
+    "BOTTEGA": 1100,
+    "PROCINO": 600,
 
     "PRODIGIO": 800,
-    "M5S": 400,
+    "M5S": 600,
 
 }
 
@@ -53,14 +62,14 @@ st.header("Liste")
 cols = st.columns((1, 1, 1))
 
 cols[0].subheader("Imposta voti")
-votes_dict = cols[0].experimental_data_editor(votes_dict)
+votes_dict = cols[0].data_editor(votes_dict)
 cols[0].write(f"Tot voti: {votes_dict.sum()}")
 
 
 coalitions_dict = {
     "CENTRODESTRA": ["FDI", "XBENE", "FORZAITALIA", "MASTRANGELO"],
     "CENTROSINISTRA": ["PD", "BOTTEGA", "PROCINO"],
-    "M5S": ["PRODIGIO", "M5S"],
+    "CENTRO": ["PRODIGIO", "M5S"],
 }
 
 coalitions_votes = {}
@@ -69,15 +78,15 @@ for c in coalitions_dict:
     for l in coalitions_dict[c]:
         coalitions_votes[c] += votes_dict[l]
 
-dhont_results_coalizioni = dhont(nSeats=16, votes=coalitions_votes, verbose=False)
+dhont_results_coalizioni = dhondt(nSeats=16, votes=coalitions_votes, verbose=True)
 
 dhont_results_by_list = dict()
 for coalizione in coalitions_dict:
     votes_coalizione_by_list = {k: votes_dict[k] for k in votes_dict.index if k in coalitions_dict[coalizione]}
-    _dhont_results_by_list = dhont(
+    _dhont_results_by_list = dhondt(
         nSeats=dhont_results_coalizioni[coalizione],
         votes=votes_coalizione_by_list,
-        verbose=False
+        verbose=True
     )
     dhont_results_by_list = dhont_results_by_list | _dhont_results_by_list
 
